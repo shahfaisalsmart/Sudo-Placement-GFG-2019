a2e74900f3067361d90880c8b9fc7bf15f5e5877
Mohammad Faisal <shahfaisalsmart1234@gmail.com>
2019-09-10 16:37:18 +0530

    Solved Successfully
    Consider a long alley with a N number of doors on one side. All the doors are closed initially. You move to and fro in the alley changing the states of the doors as follows: you open a door that is already closed and you close a door that is already opened. You start at one end go on altering the state of the doors till you reach the other end and then you come back and start altering the states of the doors again.
    
    In the first go, you alter the states of doors numbered 1, 2, 3, … , n.
    
    In the second go, you alter the states of doors numbered 2, 4, 6 …
    
    In the third go, you alter the states of doors numbered 3, 6, 9 …
    
    You continue this till the Nth go in which you alter the state of the door numbered N.
    
    You have to find the number of open doors at the end of the procedure.
    
    Input:
    
    The first line of input contains a single integer T denoting the number of test cases. Then T test cases follow. Each test case consists of one line. The line consists of a positive integer N.
    Output:
    
    Corresponding to each test case, in a new line, print the number of doors that will be open at the end of the procedure mentioned above.
    
    Constraints:
    
    1 ≤ T ≤ 100
    
    1 ≤ N ≤ 1012             
    
    
    Example:
    Input
    5
    372
    2
    100
    825625
    63542
    
    Output
    19
    1
    10
    908
    252

---
+++ Mathematics/NumberOfDoor.cpp
@@ -0,0 +1,20 @@
+// Author: Mohammad Faisal
+// Email: mohammad.faisal78612@gmail.com
+#include<bits/stdc++.h>
+#include<math.h>
+#define fast ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+using namespace std;
+int main()
+ {
+    fast
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        long long int n;
+        cin >> n;
+        cout<<floor(sqrt(n))<<endl;
+    }
+	//code
+	return 0;
+}
