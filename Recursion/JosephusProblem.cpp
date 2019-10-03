2bf421f9d79a272f28154623d29a9a60f205a252
Mohammad Faisal <shahfaisalsmart1234@gmail.com>
2019-10-03 18:21:26 +0530

    new file added

---
+++ Recursion/JosephusProblem.cpp
@@ -0,0 +1,15 @@
+// Author: Mohammad Faisal
+// Email: mohammad.faisal78612@gmail.com
+// GeeksForGeeks: 
+int josephus(int n, int k)
+{
+   //Your code here
+   if(n==1)
+   {
+       return 1;
+   }
+   else{
+   return (josephus(n-1,k)+k-1)%n+1;
+   }
+   
+}
